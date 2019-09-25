382b0776466eea0bd6f7ccfc942dad4243f969ff
tianbaoz <tianbao_zhu@trendmicro.com>
2019-09-25 13:03:41 -0500

    2019-09-25

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
+        if len(matrix) == 0:
+            return 0
+
+        ret = 0
+        dp = [[0 for x in range(len(matrix[0])+1)] for y in range(len(matrix)+1)]
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == '1':
+                    dp[i+1][j+1] = min(dp[i][j], dp[i+1][j], dp[i][j+1]) + 1
+                    ret = max(ret, dp[i+1][j+1])
+                else:
+                    dp[i+1][j+1] = 0
+
+        return ret**2
 
 """
 Input:
